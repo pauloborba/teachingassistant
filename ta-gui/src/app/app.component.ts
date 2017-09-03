722d7d903aec58ab7a6708b3ad419ce2998686ab
pauloborba <phmb@cin.ufpe.br>
2017-09-03 18:34:25 -0300

    mensagem, ao invés de alerta, para CPF já cadastrado

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -13,14 +13,17 @@ export class AppComponent {
    aluno: Aluno = {nome: "", cpf: "", email: "", github: ""};
    alunoService = new AlunoService();
    alunos: Aluno[] = [];
+   cpfduplicado: boolean = false;
 
    gravar(a: Aluno): void {
      if (this.alunoService.gravar(a)) {
        this.alunos.push(a);
        this.aluno = {nome: "", cpf: "", email: "", github: ""};
      } else {
-       this.aluno.cpf = "";
-       alert("Já existe um aluno com esse CPF");
+       this.cpfduplicado = true;
      }
+  }
+  onMove(): void {
+      this.cpfduplicado = false;
   }
 }
