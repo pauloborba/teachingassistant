1df9e2bc26d6d32872778651f96ac9e351c97d1a
pauloborba <phmb@cin.ufpe.br>
2017-09-03 18:16:32 -0300

    integração com o commit de visualização da lista de alunos cadastrados

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -13,9 +13,11 @@ export class AppComponent {
 
    aluno: Aluno = {nome: "", cpf: "", email: "", github: ""};
    alunoService = new AlunoService();
+   alunos: Aluno[] = [];
 
    gravar(a: Aluno): void {
      this.alunoService.gravar(a);
+     this.alunos.push(a);
      this.aluno = {nome: "", cpf: "", email: "", github: ""};
   }
 }
