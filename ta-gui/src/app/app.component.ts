7d30db713bfc263dd6b4c75f6d463db4f7290f44
pauloborba <phmb@cin.ufpe.br>
2017-09-03 18:13:56 -0300

    integração com o commit de botão e funcionalidade

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import { NgModule } from '@angular/core';
 
+import { Aluno } from './aluno';
+import { AlunoService } from './aluno.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-   aluno: Aluno = {nome: "", cpf: "", email: "", github: ""};
-}
 
-export class Aluno {
-  nome: string;
-  cpf: string;
-  email: string;
-  github: string;
+   aluno: Aluno = {nome: "", cpf: "", email: "", github: ""};
+   alunoService = new AlunoService();
+
+   gravar(a: Aluno): void {
+     this.alunoService.gravar(a);
+     this.aluno = {nome: "", cpf: "", email: "", github: ""};
+  }
 }
