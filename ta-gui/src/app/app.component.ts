5fadf03543250d75bdb3fed478e56c71e8bdbc37
pauloborba <phmb@cin.ufpe.br>
2017-09-03 18:16:32 -0300

    visualização

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -12,9 +12,15 @@ import { AlunoService } from './aluno.service';
 export class AppComponent {
    aluno: Aluno = {nome: "", cpf: "", email: "", github: ""};
    alunoService = new AlunoService();
+   alunos: Aluno[] = [];
 
    gravar(a: Aluno): void {
      this.alunoService.gravar(a);
+<<<<<<< HEAD
      this.aluno = {nome: "", cpf: "", email: "", github: ""};
+=======
+     this.alunos.push(a);
+     this.aluno = {nome: "", cpf: "", email: ""};
+>>>>>>> fd52d3e... visualizacao da lista de alunos cadastrados
   }
 }
