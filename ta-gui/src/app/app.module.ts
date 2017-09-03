45890aaac14872df9611cf1a69772d937208121c
pauloborba <phmb@cin.ufpe.br>
2017-09-03 18:39:08 -0300

    serviço de aluno injetado, não criado pelo componente

--- ta-gui/src/app/app.module.ts
+++ ta-gui/src/app/app.module.ts
@@ -4,15 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MetasComponent } from './metas.component';
-import { AlunosComponent } from './alunos.component';
 import { AlunoService } from './aluno.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MetasComponent,
-    AlunosComponent
   ],
   imports: [
     BrowserModule,
