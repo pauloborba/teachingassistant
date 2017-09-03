c35a054bd7e351c8d3dda81170ebd753641a4ba7
pauloborba <phmb@cin.ufpe.br>
2017-09-03 18:13:56 -0300

    botao e funcionalidade do formulario para cadastro de alunos

--- ta-gui/src/app/aluno.service.ts
+++ ta-gui/src/app/aluno.service.ts
@@ -1,38 +1,8 @@
-import { Injectable }    from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { retry, map } from 'rxjs/operators';
+import { Aluno } from './aluno';
 
-import { Aluno } from '../../../common/aluno';
-
-@Injectable()
 export class AlunoService {
-
-  private headers = new HttpHeaders({'Content-Type': 'application/json'});
-  private taURL = 'http://localhost:3000';
-
-  constructor(private http: HttpClient) {}
-
-  criar(aluno: Aluno): Observable<Aluno> {
-    return this.http.post<any>(this.taURL + "/aluno", aluno, {headers: this.headers})
-             .pipe( 
-                retry(2),
-                map( res => {if (res.success) {return aluno;} else {return null;}} )
-              ); 
+  alunos: Aluno[] = [];
+  gravar(aluno: Aluno): void {
+     this.alunos.push(aluno);
   }
-
-  atualizar(aluno: Aluno): Observable<Aluno> {
-    return this.http.put<any>(this.taURL + "/aluno",JSON.stringify(aluno), {headers: this.headers})          .pipe( 
-                retry(2),
-                map( res => {if (res.success) {return aluno;} else {return null;}} )
-              ); 
-  }
-
-  getAlunos(): Observable<Aluno[]> {
-    return this.http.get<Aluno[]>(this.taURL + "/alunos")
-              .pipe(
-                 retry(2)
-               );
-  }
-
 }
