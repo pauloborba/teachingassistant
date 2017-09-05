4128081dcdc87bddd3daf2cce69a7f73a3178795
pauloborba <phmb@cin.ufpe.br>
2017-09-05 19:24:27 -0300

    modularizando o componente principal

---
+++ ta-gui/src/app/alunos.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+ import { NgModule } from '@angular/core';
+
+  import { Aluno } from './aluno';
+ import { AlunoService } from './aluno.service';
+
+  @Component({
+   selector: 'app-root',
+   templateUrl: './alunos.component.html',
+   styleUrls: ['./alunos.component.css']
+ })
+ export class AlunosComponent implements OnInit {
+    constructor(private alunoService: AlunoService) {}
+
+    aluno: Aluno = new Aluno();
+    alunos: Aluno[];
+    cpfduplicado: boolean = false;
+
+     criarAluno(a: Aluno): void {
+      if (this.alunoService.criar(a)) {
+        this.alunos.push(a);
+        this.aluno = new Aluno();
+      } else {
+        this.cpfduplicado = true;
+      }
+    }
+
+     onMove(): void {
+       this.cpfduplicado = false;
+    }
+
+     ngOnInit(): void {
+      this.alunos = this.alunoService.getAlunos();
+    }
+
+  }
