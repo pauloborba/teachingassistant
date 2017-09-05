dfb3529257c2dc4d07d541d486a45614493a1132
pauloborba <phmb@cin.ufpe.br>
2017-09-05 19:24:27 -0300

    modularizando o componente principal

--- gui/ta-gui/src/app/app.module.ts
+++ gui/ta-gui/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MetasComponent } from './metas.component';
 import { AlunoService } from './aluno.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MetasComponent
   ],
   imports: [
     BrowserModule,
