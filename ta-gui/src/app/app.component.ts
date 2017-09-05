1f2b8f3aa9814a3f214ccdf461fdebdd28660336
pauloborba <phmb@cin.ufpe.br>
2017-09-05 18:34:00 -0300

    cadastro de metas, e clonagem de objetos para simular funcionamento do servidor

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -12,19 +12,25 @@ import { AlunoService } from './aluno.service';
 export class AppComponent {
    constructor(private alunoService: AlunoService) {}
 
-   aluno: Aluno = {nome: "", cpf: "", email: "", gitLogin: ""};
+   aluno: Aluno = new Aluno();
    alunos: Aluno[] = [];
    cpfduplicado: boolean = false;
 
-   gravar(a: Aluno): void {
-     if (this.alunoService.gravar(a)) {
+   criarAluno(a: Aluno): void {
+     if (this.alunoService.criar(a)) {
        this.alunos.push(a);
-       this.aluno = {nome: "", cpf: "", email: "", gitLogin: ""};
+       this.aluno = new Aluno();
      } else {
        this.cpfduplicado = true;
      }
-  }
-  onMove(): void {
+   }
+
+   onMove(): void {
       this.cpfduplicado = false;
-  }
+   }
+
+   atualizarAluno(aluno: Aluno): void {
+      this.alunoService.atualizar(aluno);
+   }
+
 }
