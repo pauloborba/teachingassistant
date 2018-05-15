3632919872265f2908778cc018a806a19c518522
Mario Cardoso Batista da Silva Filho <mcbsf@ug3c16.windows.cin.ufpe.br>
2018-05-15 17:06:52 -0300

    implementando feature autoavaliacao

---
+++ gui/ta-gui/src/app/autoavaliacao.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { NgModule } from '@angular/core';
+
+import { Aluno } from './aluno';
+import { AlunoService } from './aluno.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './autoavaliacao.component.html',
+  styleUrls: ['./autoavaliacao.component.css']
+})
+export class AutoAvaliacaoComponent implements OnInit {
+   constructor(private alunoService: AlunoService) {}
+
+   aluno: Aluno = new Aluno();
+   alunos: Aluno[];
+   cpfduplicado: boolean = false;
+
+   criarAluno(a: Aluno): void {
+     this.alunoService.criar(a)
+        .then(ab => {
+           if (ab) {
+              this.alunos.push(ab);
+              this.aluno = new Aluno();
+           } else {
+              this.cpfduplicado = true;
+           }
+        })
+        .catch(erro => alert(erro));
+   }
+
+   onMove(): void {
+      this.cpfduplicado = false;
+   }
+
+   ngOnInit(): void {
+     this.alunoService.getAlunos()
+         .then(as => this.alunos = as)
+         .catch(erro => alert(erro));
+   }
+
+}
