d9c6253f0a47ec652d450b6567a2b2fcd354b9a3
Mario Cardoso Batista da Silva Filho <mcbsf@cin.ufpe.br>
2018-05-17 09:53:53 -0300

    adicionando autoavaliacao feature/teste 2

--- tests-acceptance/features/alunos.feature
+++ tests-acceptance/features/alunos.feature
@@ -7,3 +7,20 @@ Given I am at the students page
 Given I cannot see a student with CPF "683" in the students list
 When I try to register the student "Paulo" with CPF "683"
 Then I can see "Paulo" with CPF "683" in the students list
+
+Scenario: Preencher avaliacao do aluno “Mario” GUI
+Given estou na página de autoavaliacao
+And consigo ver o nome “Mario” na lista de nomes
+And a meta “Entender conceitos de requisitos” esta com valor “” 
+And a meta “Especificar requisitos com qualidade” esta com valor “” 
+And a meta “Entender conceitos de gerência de configuração” esta com valor “” 
+When mudo o valor do conceito “Entender conceitos de requisitos” para “MA” 
+And mudo o valor do conceito “Especificar requisitos com qualidade” esta com valor” para “MA” 
+And mudo o valor do conceito “Entender conceitos de gerência de configuração” para “MA”
+And salvo as mudancas realizadas
+Then eu recebo uma mensagem de confirmação
+
+Scenario: Ver avaliações discrepantes com discrepância em 1 de 3 alunos GUI
+Given estou na página do professor 
+When vou para a pagina de auto avaliações discrepantes
+Then consigo ver que a “quantidade de alunos” tem valor “1(33% do total de alunos)”
--- tests-acceptance/stepdefinitions/alunos.ts
+++ tests-acceptance/stepdefinitions/alunos.ts
@@ -7,7 +7,8 @@ let sleep = (ms => new Promise(resolve => setTimeout(resolve, ms)));
 
 let sameCPF = ((elem, cpf) => elem.element(by.name('cpflist')).getText().then(text => text === cpf));
 let sameName = ((elem, name) => elem.element(by.name('nomelist')).getText().then(text => text === name));
-
+let sameMeta = ((elem, meta) => elem.element(by.name('metalist')).getText().then(text => text === meta));
+let sameDiscrepancia = ((elem, meta) => elem.element(by.name('discrepancias')).getText().then(text => text === meta));
 defineSupportCode(function ({ Given, When, Then }) {
     Given(/^I am at the students page$/, async () => {
         await browser.get("http://localhost:4200/");
@@ -36,5 +37,66 @@ defineSupportCode(function ({ Given, When, Then }) {
         var samenamecpf = allalunos.filter(elem => sameCPF(elem,cpf) && sameName(elem,name));
         await samenamecpf;
         await samenamecpf.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+});
+})
+
+defineSupportCode(function ({ Given, When, Then }) {
+    Given(/^estou na página de autoavaliacao$/, async () => {
+        await browser.get("http://localhost:4200/metas");
+        await expect(browser.getTitle()).to.eventually.equal('TaGui');
+        await $("a[name='alunos']").click();
+    })
+
+    Given(/^consigo ver o nome "([^\"]*)" na lista de nomes$/, async (name) => {
+        var allalunos : ElementArrayFinder = element.all(by.name('alunologged'));
+        await allalunos;
+        var aluno = allalunos.filter(elem => sameName(elem,name));
+        await aluno;
+        await aluno.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+    });
+    
+    Given(/^a meta “([^\"]*)” esta com valor “([^\"]*)”$/, async (goal, value) => {
+        var allmetas : ElementArrayFinder = element.all(by.name('goal'));
+        await allmetas;
+        var metas = allmetas.filter(elem =>
+                                      elem.getText().then(text => text === value));
+        await metas;
+        await metas.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+    });
+
+    When(/^mudo o valor do conceito “([^\"]*)” para “([^\"]*)”$/, async (goal, value) => {
+        await $("input[name='"+goal+"']").sendKeys(<string> value);
+    });
+
+    When(/^salvo as mudancas realizadas$/, async () => {
+        await element(by.buttonText('Salvar')).click();
+    });
+
+    Then(/^eu recebo uma mensagem de confirmação$/, async () => {
+        var confirmationMessege : ElementArrayFinder = element.all(by.name('confirmationMessege'));
+        await confirmationMessege;
+        await confirmationMessege.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
     });
 })
+
+defineSupportCode(function ({ Given, When, Then }) {
+    Given(/^estou na página do professor$/, async () => {
+        await browser.get("http://localhost:4200/teacher");
+        await expect(browser.getTitle()).to.eventually.equal('TaGui');
+        await $("a[name='alunos']").click();
+    })
+
+    When(/^vou para a pagina de auto avaliações discrepantes”$/, async (goal, value) => {
+        await $("input[name='"+goal+"']").sendKeys(<string> value);
+        await element(by.buttonText('discrepancias')).click();
+    });
+
+    Then(/^Then consigo ver que a “quantidade de alunos” tem valor “1(33% do total de alunos)”$/, async (tagName, value) => {
+        var quantidadeAlunos : ElementArrayFinder = element.all(by.name("'"+tagName+"'"));
+        await quantidadeAlunos.filter(elem =>
+                                      elem.getText().then(text => text === value));
+        await quantidadeAlunos.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+    });
+
+})
+
