aaba029a6993c5f50d567facc2ebc31e61b16a90
João Victor <joao.coutinho9@gmail.com>
2018-05-19 21:14:54 -0300

    Impossibilitando cadastro parcial de auto-avaliação

--- server/ta-server/cadastrodealunos.ts
+++ server/ta-server/cadastrodealunos.ts
@@ -18,9 +18,22 @@ export class CadastroDeAlunos {
   }
 
   atualizar(aluno: Aluno): Aluno {
-    var result: Aluno = this.alunos.find(a => a.cpf == aluno.cpf);
-    if (result) result.copyFrom(aluno);
-    return result;
+    if(this.completouAutoAvaliacao(aluno)) {
+      var result: Aluno = this.alunos.find(a => a.cpf == aluno.cpf);
+      if (result) result.copyFrom(aluno);
+      console.log("oi");
+      return result;
+     
+    } else 
+      return null;
+  }
+
+  completouAutoAvaliacao(aluno: Aluno) {
+    return !this.nullOrEmpty(aluno.metasAutoAvaliacao['requisitos']) && !this.nullOrEmpty(aluno.metasAutoAvaliacao['gerDeConfiguracao']);
+  }
+
+  nullOrEmpty(meta: string): boolean {
+    return meta == null || meta == "";
   }
 
   getAlunos(): Aluno[] {
