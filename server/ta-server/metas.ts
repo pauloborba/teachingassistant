0b2f9d41565d3d9123da01f3bfe82169d87be189
Andre Carneiro <jacn@cin.ufpe.br>
2019-06-16 16:38:51 -0300

    Adição de serviço de metas

---
+++ server/ta-server/metas.ts
@@ -0,0 +1,34 @@
+import { Meta } from '../../gui/ta-gui/src/app/meta';
+
+export class Metas {
+    metas: Meta[] = [];
+
+    criar(meta: Meta): Meta {
+        var result = null;
+        if (!this.metas.find(m => m.nome == meta.nome)) {
+            result = new Meta();
+            result.nome = meta.nome;
+            this.metas.push(result);
+        }
+        return result;
+    }
+
+    atualizar(meta: Meta, nome: string): Meta {
+        var result: Meta = this.metas.find(m => m.nome == meta.nome);
+        result.nome = nome;
+        return result;
+    }
+
+    remover(meta: Meta) : Meta {
+        const index: number = this.metas.indexOf(meta);
+        if (index !== -1) {
+            this.metas.splice(index, 1);
+        }
+        
+        return meta;
+    }
+
+    getMetas() : Meta[] {
+        return this.metas
+    }
+}
