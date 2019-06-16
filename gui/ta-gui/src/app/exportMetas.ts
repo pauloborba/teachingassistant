e9eab04d5510d7d6ab7d029f7a8c0aad3b998a6f
Henrique Mariz <ham2@cin.ufpe.br>
2019-06-16 02:39:24 -0300

    exportMetas.ts almost done

--- gui/ta-gui/src/app/exportMetas.ts
+++ gui/ta-gui/src/app/exportMetas.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AlunosComponent } from './alunos.component';
+import { Aluno } from './aluno';
+import { AlunoService } from './aluno.service';
 
 
 @Component({
@@ -8,9 +11,91 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ExportMetas implements OnInit{
-    
+    constructor(private alunoService: AlunoService) {}
+
+    public tittle: string;
+    public prova: Map<String,String[]> = new Map([
+        ["MP1", ["requisitos","gerDeConfiguracao","gerDeProjetos"]],
+        ["MP2",["SaaS","testes","projeto","refatoracao"]]
+        ]);
+    public alunos: Aluno[];
+    public option: string;
+
     ngOnInit(): void {
-        throw new Error("Method not implemented.");
+        this.alunoService.getAlunos()
+        .then(as => this.alunos = as)
+        .catch(erro => alert(erro));
+    }
+
+    columns(option:string):string{
+        var column_names="";
+        column_names = "nome,";
+
+        var conceitos = this.prova[option];
+        for(var conceito in conceitos){
+            column_names = column_names + conceito + ","
+        }
+
+        column_names = column_names.slice(0,-1);
+        column_names += "/n";
+      
+        return column_names;
+    }
+
+    rows(option:string):string{
+        var dataRows = ""
+        for(var i=0;i<this.alunos.length; i++){
+            dataRows = dataRows+ this.alunos[i].nome+",";
+            var conceitos = this.prova[option];
+            for(var _ in conceitos){
+                dataRows = dataRows + ",";
+            }
+            dataRows = dataRows.slice(0,-1);
+            dataRows += "\n";
+        }
+        return dataRows;
+    }
+
+    exportToCSV(){
+        console.log("exporting to csv")
+
+        this.alunoService.getAlunos()
+        .then(as => this.alunos = as)
+        .catch(erro => alert(erro));
+
+        console.log(this.alunos)
+
+        //TODO: get option and update tittle
+        this.option = "MP1";
+        this.tittle = this.option;
+
+        var csvContent = "";
+
+        var column_names = this.columns(this.option);
+        var dataRows = this.rows(this.option);
+       
+        csvContent = column_names + dataRows;
+        var filename = this.tittle.replace(/ /g,'')+'.csv'; //gen a filename using the title but getting rid of spaces
+        var blob = new Blob([csvContent], { "type": 'text/csv;charset=utf-8;' });
+        if (navigator.msSaveBlob) 
+        { // IE 10+
+            navigator.msSaveBlob(blob, filename);
+        } 
+        else //create a link and click it
+        {
+            var link = document.createElement("a");
+            if (link.download !== undefined) // feature detection
+            { 
+            // Browsers that support HTML5 download attribute
+            var url = URL.createObjectURL(blob); 
+            link.setAttribute("href", url);
+            link.setAttribute("download", filename);
+            link.style.visibility = 'hidden';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+            }
+        }
     }
 
 }
