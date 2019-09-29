e69b45feff77bc36ecb86ef4c8eeefd63c46d520
Vinicius Dantas <vdj@cin.ufpe.br>
2019-09-28 21:31:57 -0300

    corrigindo o diretorio para dar chery-pick certo

--- gui/ta-gui/src/app/app.component.ts
+++ gui/ta-gui/src/app/app.component.ts
@@ -1,42 +1,17 @@
 import { Component } from '@angular/core';
 import { NgModule } from '@angular/core';
 
-import { Aluno } from './aluno';
-import { AlunoService } from './aluno.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-<<<<<<< HEAD
-   constructor(private alunoService: AlunoService) {}
-
-   aluno: Aluno = new Aluno();
-   alunos: Aluno[] = [];
-   cpfduplicado: boolean = false;
-
-   criarAluno(a: Aluno): void {
-     if (this.alunoService.criar(a)) {
-       this.alunos.push(a);
-       this.aluno = new Aluno();
-     } else {
-       this.cpfduplicado = true;
-     }
-   }
-
-   onMove(): void {
-      this.cpfduplicado = false;
-   }
-
-=======
-   aluno: Aluno = {nome: "", cpf: "", email: ""};
-   alunoService = new AlunoService();
-
-   gravar(a: Aluno): void {
-     this.alunoService.gravar(a);
-     this.aluno = {nome: "", cpf: "", email: ""};
-  }
->>>>>>> 20bb097... botao e funcionalidade do formulario para cadastro de alunos
+   aluno: Aluno = {nome: "", cpf: "", email: "", github: ""};
+}
+export class Aluno {
+  nome: string;
+  cpf: string;
+  email: string;
+  github: string;
 }
--- gui/ta-gui/src/app/app.module.ts
+++ gui/ta-gui/src/app/app.module.ts
@@ -1,36 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule }   from '@angular/router';
-import { HttpModule } from '@angular/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MetasComponent } from './metas.component';
-import { AlunosComponent } from './alunos.component';
-import { AlunoService } from './aluno.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MetasComponent,
-    AlunosComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule, 
-    RouterModule.forRoot([
-      {
-        path: 'metas',
-        component: MetasComponent
-      },
-      {
-        path: 'alunos',
-        component: AlunosComponent
-      }
-    ])
+    FormsModule
   ],
-  providers: [AlunoService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
