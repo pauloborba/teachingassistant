6bf9a335f71c2668f04608bbd279be684aa52297
Gaudiot <victor.gaudiot@gmail.com>
2020-08-04 20:51:26 -0300

    adding github property to aluno class

--- common/aluno.ts
+++ common/aluno.ts
@@ -2,6 +2,7 @@ export class Aluno {
   nome: string;
   cpf: string;
   email: string;
+  github: string;
   metas: Map<string,string>;
 
   constructor() {
@@ -12,6 +13,7 @@ export class Aluno {
     this.nome = "";
     this.cpf = "";
     this.email = "";
+    this.github = "";
     this.metas = new Map<string,string>();
   }
 
@@ -25,6 +27,7 @@ export class Aluno {
     this.nome = from.nome;
     this.cpf = from.cpf;
     this.email = from.email;
+    this.github = from.github;
     this.copyMetasFrom(from.metas);
   }
 
