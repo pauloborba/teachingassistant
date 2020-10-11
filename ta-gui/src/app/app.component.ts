4fb8f06071059acf9f87ca14b44bae4582deefbc
gabrieldapaz <mgpp@cin.ufpe.br>
2020-10-11 10:46:54 -0300

    adicionando o campo github

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -4,14 +4,15 @@ import { NgModule } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-   aluno: Aluno = {nome: "", cpf: "", email: ""};
+  aluno: Aluno = { nome: '', cpf: '', email: '', github: '' };
 }
 
 export class Aluno {
   nome: string;
   cpf: string;
   email: string;
+  github: string;
 }
