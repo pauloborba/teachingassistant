5ae0850e213173e300c9ebe31f5633f3b9f22d3d
rodrigo <rfop@cin.ufpe.br>
2020-11-11 19:36:37 -0300

    coloquei test-accpetance, common e mundancas no ta-server do servidor

--- ta-server/ta-server.ts
+++ ta-server/ta-server.ts
@@ -1,13 +1,24 @@
-import { OPENSSL_VERSION_NUMBER } from 'constants';
+
 import express = require('express');
+
+import bodyParser = require("body-parser");
 
 var taserver = express();
 
-var alunos = [{nome:'Paulo',cpf:'683',email:'phmb@cin.br',metas:{'requisitos':'MA'}},{nome:'Mariana',cpf:'456',email:'@mcb@cin.br',metas:{'requisitos':'MPA'}}];
+var allowCrossDomain = function(req: any, res: any, next: any) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
 
-taserver.get('/', function (req, res) {
-  res.send(alunos);
+taserver.use(allowCrossDomain);
+taserver.use(bodyParser.json());
+
+taserver.get('/', function(req,res) {
+  res.send("Oi, essa é uma resposta padrão do servidor do Teaching Assistant 2020-3")
 })
+
 
 taserver.listen(3000, function () {
   console.log('Example app listening on port 3000!');
