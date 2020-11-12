fde3ce9372429f635fb6797e887b81467936b4c8
Maria Augusta <mamb2@cin.ufpe.br>
2020-11-12 18:27:12 -0300

    Gui Metas

---
+++ ta-gui/src/app/metas.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { NgModule } from '@angular/core';
+
+import { Aluno } from '../../../common/aluno';
+import { AlunoService } from './aluno.service';
+
+  @Component({
+   selector: 'metas',
+   templateUrl: './metas.component.html',
+   styleUrls: ['./metas.component.css']
+ })
+ export class MetasComponent implements OnInit {
+    constructor(private alunoService: AlunoService) {}
+
+    alunos: Aluno[];
+
+    atualizarAluno(aluno: Aluno): void {
+  
+    }
+
+    ngOnInit(): void {
+      
+  }
