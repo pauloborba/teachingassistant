428520b52b4fbc2b33dfaa853de96f804c84e5db
Elaine <ecf2@cin.ufpe.br>
2020-11-12 19:20:32 -0300

    Adicionando esqueleto de Turma e Turmas

---
+++ ta-server/turmas.ts
@@ -0,0 +1,26 @@
+import { Turma } from './turma';
+
+export class Turmas {
+    turmas: Turma[ ] = [ ]
+
+
+    cadastrarTurma(turma: Turma): Turma{
+
+    }
+
+    removerTurma(descricao: string): Turma{
+
+    }
+
+    atualizarTurma(turma: Turma): Turma{
+
+    }
+    
+    compararTurmas(turmas: Turma []): any{
+
+    }
+
+    getTurma(descricao: string): Turma{
+
+    }
+}
