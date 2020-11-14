716f72e496c055ad6201b64f8ba7ce261d6505ed
Matheus <mfs5@cin.ufpe.br>
2020-11-14 13:12:23 -0300

    adicionando atributo de array de roteiros

--- ta-server/cadastroderoteiros.ts
+++ ta-server/cadastroderoteiros.ts
@@ -1,7 +1,8 @@
 import  {Roteiro } from './roteiro'
 
 export class CadastroDeRoteiros{
-
+       roteiros: Roteiro[] = [];
+       
        cadastrarRoteiro (roteiro: Roteiro): void{};
 
        removerRoteiro (descricao: String ): void{};
