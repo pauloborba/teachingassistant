889339f5c9d064d06017f541b8734c3ff9fe7b47
matheus hopper <mhjc@cin.ufpe.br>
2020-11-17 15:40:50 -0300

    criação do modulo aluno.ts e modificações em matricula.ts

---
+++ common/aluno.ts
@@ -0,0 +1,12 @@
+export class Aluno {
+    nome: String;
+    email: String;
+
+    getNome(): String {
+        return this.nome
+    }
+
+    getEmail(): String {
+        return this.email
+    }
+}
